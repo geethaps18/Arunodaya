1c15d03730d29715314c1783b4e0a77767cbd341
Geetha P S <geethaps2001@gmail.com>
2026-04-07 21:34:00 +0530

    main

--- global.d.ts
+++ global.d.ts
@@ -2,9 +2,11 @@ declare module '*.css';
 declare module '*.scss';
 declare module '*.sass';
 
-// 🔥 ADD THESE (IMPORTANT)
 declare module 'swiper/css';
 declare module 'swiper/css/pagination';
 declare module 'swiper/css/navigation';
 declare module 'swiper/css/scrollbar';
 declare module 'swiper/css/zoom';
+
+// 🔥 ADD THIS
+declare module 'bwip-js';
